8fe541e075054ab27c2706bef51a8b3cf358c8d0
qai <qai@mit.edu>
2023-01-14 23:24:44 -0500

    init migrate from `ord-epv`

---
+++ betterproto.sh
@@ -0,0 +1,21 @@
+# make sure protoc compiler is installed
+# on debian do `sudo apt install protobuf-compiler`
+whereami=$PWD
+
+mkdir ord_betterproto
+cd ord_betterproto || exit
+touch __init__.py
+for protoname in reaction dataset
+do
+  wget https://raw.githubusercontent.com/open-reaction-database/ord-schema/main/ord_schema/proto/$protoname.proto
+  if [[ $protoname == dataset ]]
+  then
+    sed -i 's/ord_schema\/proto\/reaction.proto/reaction.proto/' $protoname.proto
+  fi
+done
+
+mkdir -p ord
+protoc -I . --python_betterproto_out=ord dataset.proto --experimental_allow_proto3_optional
+mv ord/ord/__init__.py ./
+rm -rf ord
+cd "$whereami" || exit
