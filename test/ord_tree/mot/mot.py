1cfe752926ccf2cd73789851207a3a78090fb3b5
qai <qai@mit.edu>
2023-01-25 11:40:00 -0500

    - update examples for mot and mtt

--- test/ord_tree/mot/mot.py
+++ test/ord_tree/mot/mot.py
@@ -1,13 +1,11 @@
 from ord_betterproto import Reaction
-from ord_betterproto.message_object_tree import message_object_to_message_object_tree, \
-    message_object_tree_to_message_object
-from ord_betterproto.utils import write_dot
+from ord_tree.tree_obj import message_object_to_message_object_tree, message_object_tree_to_message_object
+from ord_tree.utils import write_dot, read_file
 
 if __name__ == '__main__':
     # write tree of a reaction from ord examples
     # https://github.com/open-reaction-database/ord-schema/blob/main/examples/submissions/3_Liu_Copper_OrgSyn/example_liu.ipynb
-    with open("../ord_betterproto/reaction_ord.json", "r") as f:
-        json_string = f.read()
+    json_string = read_file("../../ord_betterproto/reaction_ord.json")
     r = Reaction().from_json(json_string)
     tree = message_object_to_message_object_tree(r)
     write_dot(tree, "tree_liu.dot")
--- test/ord_tree/mtt/mtt.py
+++ test/ord_tree/mtt/mtt.py
@@ -1,6 +1,6 @@
 from ord_betterproto import Reaction, Compound, ReactionInput, ReactionWorkup
-from ord_betterproto.message_type_tree import message_type_to_message_type_tree
-from ord_betterproto.utils import write_dot
+from ord_tree.tree_type import message_type_to_message_type_tree
+from ord_tree.utils import write_dot
 
 if __name__ == '__main__':
     # write tree of a Message type in dot format
