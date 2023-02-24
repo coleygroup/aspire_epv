adf72d415d0bb27262ef3700da272df20935a4fa
qai <qai@mit.edu>
2023-02-24 09:58:53 -0500

    - relocate cyto app module - add sample data for mot

--- cyto_app/fixtures.py
+++ cyto_app/fixtures.py
@@ -3,6 +3,9 @@ import json
 import os.path
 from typing import TypedDict
 
+from ord_betterproto import Reaction
+from ord_tree.utils import read_file
+
 _this_dir = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -16,7 +19,9 @@ class MttData(TypedDict):
 
 
 MttDataDict = dict()
-for json_file in glob.glob(f"{_this_dir}/../json_mtt/*.json"):
+for json_file in glob.glob(f"{_this_dir}/json_mtt/*.json"):
     with open(json_file, "r") as f:
         data = MttData(**json.load(f))
         MttDataDict[data['name']] = data
+
+SampleReactionInstance = Reaction().from_json(value=read_file(f"{_this_dir}/ORD_Reaction.json"))
